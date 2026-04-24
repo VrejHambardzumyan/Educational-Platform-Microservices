467b49c763704c89aac2c39a8bae22bf1f252511
VrejHambardzumyan <vrezhambardzumyan04@gmai.com>
2026-04-24 16:20:56 +0400

    add user role migration

---
+++ UserManagementService/Migrations/20260424122021_AddUserRole.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagementService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "OtpEnabled",
+                schema: "user_management",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OtpEnabled",
+                schema: "user_management",
+                table: "users");
+        }
+    }
+}
